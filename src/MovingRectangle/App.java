44aa28f6cc53cfe3dd9559ac73d0324224b9b6ef
Philippe P. Lafond <philp.lafond@gmail.com>
2024-09-18 13:49:24 -0400

    Feat: Made new game

---
+++ src/MovingRectangle/App.java
@@ -0,0 +1,9 @@
+package MovingRectangle;
+
+public class App {
+
+    public static void main(String[] args) {
+        MovingRectangleGame game = new MovingRectangleGame();
+        game.start();
+    }
+}
---
+++ src/MovingRectangle/MovingRectangleGame.java
@@ -0,0 +1,22 @@
+package MovingRectangle;
+
+import Engine.Canvas;
+import Engine.Game;
+
+public class MovingRectangleGame extends Game {
+
+    @Override
+    protected void initialize() {
+
+    }
+
+    @Override
+    protected void update() {
+
+    }
+
+    @Override
+    protected void draw(Canvas canvas) {
+
+    }
+}

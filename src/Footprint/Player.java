f54d747500089a23f32f0479c04fbb1ad01b6779
Philippe P. Lafond <philp.lafond@gmail.com>
2024-10-02 13:41:16 -0400

    Refactor: Extract ControllableEntity abstract class

--- src/Footprint/Player.java
+++ src/Footprint/Player.java
@@ -1,26 +1,24 @@
 package Footprint;
 
 import Engine.Canvas;
-import Engine.MovableEntity;
+import Engine.ControllableEntity;
 
 import java.awt.*;
 
-public class Player extends MovableEntity {
-    private GamePad gamePad;
-
+public class Player extends ControllableEntity {
     public Player(GamePad gamePad) {
-        this.gamePad = gamePad;
+        super(gamePad);
         teleport(200, 200);
         resize(20, 60);
         setSpeed(3);
     }
 
+    @Override
     public void update() {
-        if (gamePad.isMoving()) {
-            move(gamePad.getDirection());
-        }
+        moveWithController();
     }
 
+    @Override
     public void draw (Canvas canvas) {
         canvas.drawRectangle(this, Color.WHITE);
     }

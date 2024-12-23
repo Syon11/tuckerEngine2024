a5064a754ae3fb7b9e599c6aac317ce1762c2f84
Philippe P. Lafond <philp.lafond@gmail.com>
2024-12-23 09:53:04 -0500

    Change for right menu coordinates.

--- src/main/java/Pokemon/Battle/BattleMenu.java
+++ src/main/java/Pokemon/Battle/BattleMenu.java
@@ -47,17 +47,17 @@ public class BattleMenu {
     public void draw(Canvas canvas, BattleState battleState) {
         switch (battleState) {
             case SELECT_ACTION:
-                canvas.drawString("1. Fight", 200, 200, Color.WHITE); //600 400
-                canvas.drawString("2. Bag", 200, 210, Color.WHITE); // 600 410
-                canvas.drawString("3. Pokemon", 200, 220, Color.WHITE); // 600 420
-                canvas.drawString("4. Run", 200, 230, Color.WHITE); // 600 430
+                canvas.drawString("1. Fight", 600, 500, Color.WHITE); //600 400
+                canvas.drawString("2. Bag", 600, 510, Color.WHITE); // 600 410
+                canvas.drawString("3. Pokemon", 600, 520, Color.WHITE); // 600 420
+                canvas.drawString("4. Run", 600, 530, Color.WHITE); // 600 430
                 break;
             case SELECT_MOVE:
                 for (int i = 0; i < actions.length; i++) {
                     if (actions[i] == null) {
                         break;
                     }
-                    canvas.drawString((i + 1) + ". " + actions[i].getName(), 200, 200 + (i * 10), actions[i].getRemainingPP() > 0 ? Color.WHITE : Color.RED);
+                    canvas.drawString((i + 1) + ". " + actions[i].getName(), 200, 500 + (i * 10), actions[i].getRemainingPP() > 0 ? Color.WHITE : Color.RED);
                 }
                 break;
             case SELECT_ITEM:
